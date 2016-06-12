7376a9c459c76a8c6e9a5f7429a259148c73a9b8
Amitesh Singh <singh.amitesh@gmail.com>
2016-06-12 21:05:14 +0530

    dbg_util: fix compilation

--- lib/dbg_util/dbg_util.c
+++ lib/dbg_util/dbg_util.c
@@ -1,11 +1,10 @@
 #include "dbg_util.h"
 
-void blink_led(int8_t port, int8_t no,
-               long timeout)
+void blink_led(int8_t port, int8_t no)
 {
    port |= (1 << no);
-   _delay_ms(timeout);
+   _delay_ms(100);
    port &= ~(1 << no);
-   _delay_ms(timeout);
+   _delay_ms(100);
 }
 
--- lib/dbg_util/dbg_util.h
+++ lib/dbg_util/dbg_util.h
@@ -4,7 +4,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-// Usage: blink_led(PORTB, PB0, 100);
-void blink_led(int8_t port, int8_t no, long timeout_ms);
+// Usage: blink_led(PORTB, PB0);
+void blink_led(int8_t port, int8_t no);
 
 #endif
